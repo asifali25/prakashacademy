b7dc85b5660d294175592ed06c9c6f0ee059d646
asifali25 <asifaliweb2@gmail.com>
2024-01-22 18:07:05 +0530

    Robots.txt file fixed

--- src/app/robots.js
+++ src/app/robots.js
@@ -2,7 +2,13 @@ export default function robots() {
   return {
     rules: {
       userAgent: "*",
-      allow: "/",
+      allow: "/aboutus",
+      allow: "/admissionguide",
+      allow: "/biruniversity",
+      allow: "/contact",
+      allow: "/gallary",
+      allow: "/satyami",
+      allow: "/satyamlaw",
       disallow: "/private/",
     },
     sitemap: "https://prakashacademy.net/sitemap.xml",
