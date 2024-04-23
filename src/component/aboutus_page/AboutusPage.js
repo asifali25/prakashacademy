ced74e215ff697e70ac568e111a72c5657a75db8
asifali25 <asifaliweb2@gmail.com>
2024-04-23 21:26:45 +0530

    About us text has been changed

--- src/component/aboutus_page/AboutusPage.js
+++ src/component/aboutus_page/AboutusPage.js
@@ -12,62 +12,42 @@ function AboutusPage() {
             <span style={{ color: "#005ee1", fontWeight: "bold" }}>Us</span>
           </h1>
           <p className={Styles.aboutPara}>
-            Welcome to <b>Prakash Academy</b>, where education meets experience!
-            With a rich legacy spanning three decades, Prakash Academy has been
-            a beacon of knowledge and a guiding force for students on their
-            academic journey. Our commitment to excellence and passion for
-            education have established us as a trusted name in coaching.
+            <b>Prakash Academy</b> was started in 1993 even today It is
+            continusously making her valuable contirbution in education sector
+            in the same place thousand of student have been passed in these 30
+            years our Academy was registered in 2011 as a East District firm{" "}
+            {"[394]"} 2011.
             <br></br>
             <br></br>
-            <b>30 Years of Expertise</b>:<br></br> Since our inception 30 years
-            ago, Prakash Academy has been dedicated to providing top-notch
-            coaching services. Over the years, we have honed our teaching
-            methodologies and adapted to the evolving educational landscape,
-            ensuring that our students receive the best possible guidance.
+            <p>We have authorized Information center of reliable Institute:</p>
+            <p>
+              {"(1)"} Bir Tikendrajit University{" "}
+              {
+                "[state private university Recognized by UGC, BCI, RCI, PCI, AIV]"
+              }
+              .
+            </p>
             <br></br>
+            <p>
+              {"(2)"} Satyam Law College{" "}
+              {
+                "[Affiliated to CCS University Meerut & Approved by Bar Council of India]"
+              }
+              .
+            </p>
             <br></br>
-            <b>Our Mission:</b>
-            <br></br> At Prakash Academy, our mission is to empower students
-            with the knowledge and skills they need to excel in their academic
-            pursuits. We believe in fostering a conducive learning environment
-            that encourages curiosity, critical thinking, and a lifelong love
-            for learning.<br></br>
+            <p>
+              {"(3)"} Satyam Pvt. ITI{" "}
+              {
+                "[Affiliated to by NCVT, Delhi (National Council) of vocational Training Delhi]"
+              }
+              .
+            </p>
             <br></br>
-            <b>Qualified and Experienced Faculty</b>:<br></br> Our team of
-            educators comprises highly qualified and experienced professionals
-            who are passionate about teaching. They bring not only academic
-            expertise but also a commitment to nurturing the potential of each
-            student. With a student-centric approach, our faculty members go
-            beyond textbooks, aiming to instill a holistic understanding of
-            subjects.
-            <br></br>
-            <br></br>
-            <b>Comprehensive Coaching Programs</b>:<br></br> Whether preparing
-            for competitive exams or seeking academic support, Prakash Academy
-            offers a range of comprehensive coaching programs. From personalized
-            tutoring to group classes, we tailor our offerings to meet the
-            diverse needs of our students. Our well-structured curriculum,
-            coupled with modern teaching techniques, ensures effective learning
-            outcomes.<br></br>
-            <br></br>
-            <b>State-of-the-Art Facilities</b>:<br></br> Prakash Academy is
-            equipped with state-of-the-art facilities, creating an optimal
-            learning environment. Our classrooms are designed to facilitate
-            interactive learning, and we leverage technology to enhance the
-            educational experience. We believe that a conducive and comfortable
-            setting is crucial for effective learning.<br></br>
-            <br></br>
-            <b>Success Stories</b>:<br></br> Our success is measured by the
-            accomplishments of our students. Over the years, Prakash Academy has
-            witnessed numerous success stories, with our alumni achieving
-            remarkable feats in various fields. We take pride in being a part of
-            their educational journey and contributing to their success.
-            <br></br>
-            <br></br>
-            Join <b>Prakash Academy</b> on a journey of academic excellence,
-            where experience meets innovation, and students are empowered to
-            reach their full potential. Explore the possibilities with us and
-            let education be the key to unlocking your future.
+            <p>
+              Many passed out students have become Chartered Accounts, Cost And
+              Management Accounts , Company secretaries and Government Officers.
+            </p>
           </p>
         </div>
       </div>
